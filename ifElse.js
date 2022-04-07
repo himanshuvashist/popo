66b4db3b839fa1c1376bc993d71bd7de1cf98475
himanshu <himanshu.vashist@ikonwork.com>
2022-04-07 10:46:22 +0530

    if else questions

---
+++ ifElse.js
@@ -0,0 +1,76 @@
+// js only look up for 6 falsy values , other than that all values are truthy values.
+
+// try to guess weather the if condition will have truthy or falsy values
+
+
+
+// 1
+if(true){
+    console.log('shall i run 1')
+}
+
+// 2
+if('wooo'){
+    console.log('shall i run 2')
+}
+
+// 3
+if(undefined){
+    console.log('shall i run 3')
+}
+
+// 4
+if('hoi hoi'){
+    console.log('shall i run 4')
+}
+
+// 5
+if(null){
+    console.log('shall i run 5')
+}
+
+// 6
+if('null'){
+    console.log('shall i run 6')
+}
+
+// 7
+if(""){
+    console.log('shall i run 7')
+}
+
+// 8
+if(4){
+    console.log('shall i run 8')
+}
+
+// 9
+if({}){
+    console.log('shall i run 9')
+}
+
+// 10
+if({name:'vijay'}){
+    console.log('shall i run 10')
+}
+
+// 11
+if(0){
+    console.log('shall i run 11')
+}
+
+// 12
+if([]){
+    console.log('shall i run 12')
+}
+
+// 13
+if([]){
+    console.log('shall i run 13')
+}
+
+if([] || []){
+    console.log('shall i run 14')
+}
+
+
