0ce2d2f2b261e587694a479659241af3a684574a
himanshu <himanshu.vashist@ikonwork.com>
2022-04-08 11:58:46 +0530

    adding 5 function questions

---
+++ functions.js
@@ -0,0 +1,122 @@
+// 1
+// add these number and show it in alert
+
+// TODO,make fun here **********************
+
+
+
+
+
+
+
+addTheseNumbers(1,5,6)
+addTheseNumbers(1,15,86)
+addTheseNumbers(1,50,56)
+addTheseNumbers(1,56,6)
+addTheseNumbers(15,5,30)
+
+
+
+// 2
+// show each values in array in the console
+// TODO,make fun here *********************
+
+
+
+
+
+
+
+printAllTheseNumbers([3,23,23,45,2,1,0,-4])
+printAllTheseNumbers([34,23,13,45,2,1,90,-48])
+printAllTheseNumbers([3,23,3,4,2,1,0,-9])
+printAllTheseNumbers([30,2,23,45,2,1,0,-43])
+printAllTheseNumbers([3,23,23,4,2,1,0,-42])
+
+
+
+// 3
+// show each character in the string inside the console
+// TODO,make fun here *********************
+
+
+
+
+
+
+printAllTheseCharacters('thisisastring')
+printAllTheseCharacters('second string')
+printAllTheseCharacters('sukhi yo')
+printAllTheseCharacters('random string.')
+printAllTheseCharacters('hi i am fifth string')
+
+
+
+
+
+// 4
+// insert the arguments in the array
+var a = []
+// TODO,make fun here *********************
+
+
+
+
+
+insertMeInArray('🌚')
+insertMeInArray('🌞')
+insertMeInArray('⛄')
+insertMeInArray('❄️')
+insertMeInArray('💮')
+
+// checking if all the values have been inserted or not
+console.log(a)
+
+
+
+
+// 5
+var a = []
+// here we have to insert in the array a which are greater than 50,so below we have written the algorithm
+// for it but it is incomplete , complete it by writting  a function which gives truthy and falsy values
+// after checking the number.
+
+
+// TODO,make fun here *********************
+
+
+
+
+
+if(amIGreaterThen50(-9)){
+    a.push(-9)
+}
+if(amIGreaterThen50(10)){
+    a.push(10)
+}
+if(amIGreaterThen50(19)){
+    a.push(19)
+}
+if(
+amIGreaterThen50(15)){
+    a.push(15)
+}
+if(amIGreaterThen50(80)){
+    a.push(80)
+}
+
+var zOne = 90
+if(amIGreaterThen50(zOne)){
+    a.push(zOne)
+}
+
+var zTwo = 888
+if(amIGreaterThen50(zTwo)){
+    a.push(zTwo)
+}
+
+
+
+
+
+
