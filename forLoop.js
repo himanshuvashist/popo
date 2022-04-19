38d9d8aa02c6486e3dcb64594bc17ce2dc335bc5
himanshu <himanshu.vashist@ikonwork.com>
2022-04-19 11:25:33 +0530

    add files

---
+++ forLoop.js
@@ -0,0 +1,139 @@
+// question 1 :- 
+// Sum of this number '
+var a = [1,2,3,4]     // output should be 10
+
+// **TODO here**
+
+//******************
+// question 2 :-
+//Print the numbers 0 - 20  
+
+// **TODO here**
+
+
+
+//****************
+//question 3:-
+//Print only the ODD values from 3 - 29
+
+// **TODO here**
+
+
+// *************
+//question 4:-
+//Print the EVEN numbers 12 down to -14 in descending 
+
+// **TODO here**
+
+
+//***************
+//question 5:-
+//Print the numbers 50 down to 20 in descending order, but only if the numbers are multiples of 3.
+
+// **TODO here**
+
+
+
+//***************
+//question 6:-
+//the array [1, 5, 'LC101', 'blue', 42]
+//Print each element of the array to a new line.
+
+// **TODO here**
+
+
+//**************
+//question 7:-
+// Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+
+// **TODO here**
+
+
+//**************
+//question 8:-
+// Define an empty evens array to hold the even numbers and an odds array for the odd numbers.
+
+// **TODO here**
+
+
+
+//***************
+//question 9:-
+// print the number from 10 to 1 
+
+// **TODO here**
+
+
+//*****************
+//question 10:-
+// print the number from 100 to -10 using 5 no gap in between  i.e output should be like (100,95,90...-10)
+
+// **TODO here**
+
+
+//*****************
+//question 11:-
+//print each value of array  in new line
+
+// **TODO here**
+
+
+//*****************
+//question 12:-
+//print the output of the array
+// Example output: 
+//  276 + 351 = 627
+
+// **TODO here**
+
+
+//*****************
+//*****************
+//question 13:-
+//print the number from 5 to 95 using 5 no gap in between  i.e output should be like (5,10,15...95)
+
+// **TODO here**
+
+
+//*****************
+//question 14:-
+//print the number from 101 to -10  i.e output should be like (101,100,95....-10)
+
+// **TODO here**
+
+
+//*****************
+//question 15:-
+//print the 7 table from 0 to 77 and output be like(0,7,14....77)
+
+// **TODO here**
+
+
+//*****************
+//question 16:-
+//print the 7 table from 7 to 77 and output be like(7,14,21....77)
+
+// **TODO here**
+
+
+
+//*****************
+//question 17:-
+//print the 2 to 128 i.e double of the value and output be (2,4,8...128)
+
+// **TODO here**
+
+
+//*****************
+//question 18:-
+//print the 128 to 2 i.e double of the value and output be (128,64,32...2)
+
+// **TODO here**
+
+
+//*****************
+//question 19:-
+//print the odd value 
+// var a = [1,53,5657,565454,32423423,785786,9898989]
+
+// **TODO here**
---
+++ functionsOne.js
@@ -0,0 +1,78 @@
+//*****************
+// question 1 
+// using functiuon concept print the array length
+// var arr_1 = [1,53,5656,565454,32423423,3434,353,349,01,234,23447];
+// var arr_2 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; 
+// var arr_3 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; 
+
+
+//*****************
+// question 2 
+// Q. make a function which take two arguments and return their addition, (argument types are number)
+// EX - (2,3) -> 5        (5,4) -> 9
+
+
+//******************
+// question 3 
+// Q.make a function which takes the array in its argument and print all values inside
+// EX - ([4,7,8]) -> 4 7 8 
+
+//******************
+// question 4 
+//displayName using the  function
+// var names = [ 'Mehvish', 'John', 'Henry', 'Thomas'];
+
+// *****************
+// question 5 
+// Q. make a function which takes in the array and print all values inside in reverse
+// EX - ([4,7,8]) -> 8 7 4
+
+// ******************
+// question 6 
+// Q. make a function which takes a string value in its argument and return 'lol' if the argument === 'l' else return 'other' for any other values 
+// EX - ('t') -> 'other'      ('random') -> 'other'       ('l') -> 'lol'    (7) -> 'other'
+
+
+// ******************
+// question 7 
+// Q. make a function which take two arguments and return their concatination, (argument types could be number or string)
+// EX - ('one','two') -> 'onetwo'     (4,5) -> '45'    ('yo',8)   -> 'yo8'
+
+// *****************
+// question 8
+// Q. make a function which take number in its argument, number could be between 0 to 24, 
+//if we give it value  between 5 to 12 its gives us 'morning' , 
+//if value's between 12 - 17 it gives us 'afternoon' 
+//if value is between 17 - 21 it gives us 'evening' 
+//and if it is between 21-5 it gives us 'night'
+
+// **TODO here**
+
+
+// *****************
+// question 9 
+//Q. make a function which takes in the array and return a new array in which every item in it is doubled
+// EX - [3,5,6] -> [6,10,12]
+
+// **TODO here**
+
+
+//******************
+// question 10 
+//Q. make a funciton which takes in the array and return a new array saying 'yes' or 'no' if the number is prime it says 'yes' else 'no'
+// EX - [4,2,3,6,11] -> ['no','yes','yes','no','yes']
+
+// **TODO here**
+
+
+// var n=  [4,2,3,6,11] ;
+// var b = []
+
+//*****************
+// question 11 
+// Q. First make a array containing numbers [1,2....100] now, pass this to a function which will tell us the following
+//  if the number is 'even' console 'even' , if odd console 'odd',
+//if 'even and prime' print 'even and prime'
+// if 'odd and prime' return 'odd and prime'
+
+// **TODO here**
