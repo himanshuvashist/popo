2b953a4b344442f03c4a0064dd8e3fa9be7aacc4
Himanshu vashist <35025815+himanshuvashist@users.noreply.github.com>
2022-05-18 22:51:09 +0530

    Update index.js

--- prac/17may/taskTwo/index.js
+++ prac/17may/taskTwo/index.js
@@ -1,6 +1,1 @@
-// suppose we got these messages from the API 
-// *******************
-const messages = [{msg:'hi',time:'12pm',list:['orange','lemon']},{msg:'hello',time:'1pm',list:['papaya','orange','ice']}]
-// ******************
 
-// ******TODO*******
