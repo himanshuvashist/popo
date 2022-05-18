1a786ebfab91f6c8aa1676eacf522983df4068e7
Himanshu vashist <35025815+himanshuvashist@users.noreply.github.com>
2022-05-18 22:51:27 +0530

    Update index.js

---
+++ prac/17may/taskOne/index.js
@@ -0,0 +1,6 @@
+// suppose we got these messages from the API 
+// *******************
+const messages = [{msg:'hi',time:'12pm',list:['orange','lemon']},{msg:'hello',time:'1pm',list:['papaya','orange','ice']}]
+// ******************
+
+// ******TODO*******
